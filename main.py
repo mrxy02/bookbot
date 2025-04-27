16e1d8d831124328c4387b80cabb08bea35ddd8c
Marcel Runzer <marcel.runzer@gmail.com>
2025-04-27 19:00:15 +0200

    finished project

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from stats import counter, letter_counter, splitter
-
+import sys 
 def get_book_text(path):
     with open(path) as file:
         file.contents = file.read()
     return file.contents
 
 def main():
-    file_path = "/Users/marcelrunzer/workspace/boot.dev/bookbot/books/frankenstein.txt"
+    file_path = "/Users/marcelrunzer/workspace/boot.dev/bookbot/" + sys.argv[1]
     texts = get_book_text(file_path)
     count_words = counter(texts)
     all_letters = letter_counter(texts)
@@ -20,4 +20,7 @@ def main():
         if letter["char"].isalpha():
             print(f"{letter['char']}: {letter['num']}")
     print("============= END ===============")
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 main()
